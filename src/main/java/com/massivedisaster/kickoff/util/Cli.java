6a63c95bddc6764fcac59ca2d3e9cc1bd9026457
Jorge Costa <j.costa90@gmail.com>
2017-05-29 19:25:27 +0100

    Removed unused option and code format.

--- src/main/java/com/massivedisaster/kickoff/util/Cli.java
+++ src/main/java/com/massivedisaster/kickoff/util/Cli.java
@@ -16,73 +16,72 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 @SuppressWarnings("deprecation")
-public class Cli{
+public class Cli {
 
-	private static final Logger log = Logger.getLogger(Cli.class.getName());
+    private static final Logger log = Logger.getLogger(Cli.class.getName());
 
-	private String[] args = null;
-	private Options options = new Options();
+    private String[] args = null;
+    private Options options = new Options();
 
-	public Cli(String[] args) {
+    public Cli(String[] args) {
 
-		this.args = args;
+        this.args = args;
 
-		options.addOption(new Option("h", "help", false, "Show Help."));
-		options.addOption(new Option("u", "update", false, "Update the template project."));
+        options.addOption(new Option("h", "help", false, "Show Help."));
         Option optGenerate = new Option("g", "generate", true, "Generate a new project based on a configuration file.");
         optGenerate.setArgName("configuration file");
-		options.addOption(optGenerate);
-		options.addOption(new Option("o", "open", false, "Open a website to create a new project configuration file."));
-	}
+        options.addOption(optGenerate);
+        options.addOption(new Option("o", "open", false, "Open the GitHub repository."));
+    }
 
-	 public void parse() {
-		CommandLineParser parser = new BasicParser();
+    public void parse() {
+        CommandLineParser parser = new BasicParser();
 
-		CommandLine cmd = null;
-		try {
-			cmd = parser.parse(options, args, true);
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args, true);
 
-			if (cmd.hasOption("h")){
-				help();
-			} else if (cmd.hasOption("o")) {
-				open();
-			} else if(!cmd.hasOption("g")){
-				log.log(Level.SEVERE, "Missing g option");
-				help();
-			}
+            if (cmd.hasOption("h")) {
+                help();
+            } else if (cmd.hasOption("o")) {
+                open();
+            } else if (!cmd.hasOption("g")) {
+                log.log(Level.SEVERE, "Missing g option");
+                help();
+            }
 
-		} catch (ParseException e) {
-			help();
-		}
-	}
+        } catch (ParseException e) {
+            help();
+        }
+    }
 
-	 public CommandLine getOptions(){
-		CommandLineParser parser = new BasicParser();
-		try {
-			return parser.parse(options, args, true);
-		} catch (ParseException e) {
-			help();
-			return null;
-		}
+    public CommandLine getOptions() {
+        CommandLineParser parser = new BasicParser();
+        try {
+            return parser.parse(options, args, true);
+        } catch (ParseException e) {
+            help();
+            return null;
+        }
 
-	}
+    }
 
-	private void help() {
-		// This prints out some help
-		HelpFormatter formater = new HelpFormatter();
+    private void help() {
+        // This prints out some help
+        HelpFormatter formater = new HelpFormatter();
         formater.printHelp("kickoff", options);
-		System.exit(0);
-	}
+        System.exit(0);
+    }
 
-	private void open() {
-		try {
+    private void open() {
+        try {
             System.out.println("Opening an online platform to create a configuration file...");
             Desktop.getDesktop().browse(new URI(Const.WEBSITE_URL));
         } catch (IOException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		System.exit(0);
-	}
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
 }
